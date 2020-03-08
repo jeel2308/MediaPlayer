02576cf8713d53ef77b92d442e75b4fe7bdf51de
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-03-09 01:20:24 +0530

    improve ui for shuffle/loop button

--- src/js/ShuffleOrLoop.js
+++ src/js/ShuffleOrLoop.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import { MdShuffle, MdRepeatOne } from "react-icons/md";
+import ReactTooltip from "react-tooltip";
 import "../css/buttons.css";
 const ShuffleOrLoop = ({ loop, handleLoop }) => {
   return (
-    <div onClick={handleLoop} className="wrapper">
+    <div
+      onClick={handleLoop}
+      className="wrapper"
+      data-tip={loop ? "loop mode" : "shuffle mode"}
+      data-effect="solid"
+      data-place="top"
+    >
+      <ReactTooltip />
       <IconContext.Provider value={{ className: "loop hover" }}>
         {loop ? <MdRepeatOne /> : <MdShuffle />}
       </IconContext.Provider>
