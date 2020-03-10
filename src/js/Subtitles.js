db35370bc91e80511c190b74b6ed6f7307624760
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-03-10 20:49:03 +0530

    improved ui of Caption button

--- src/js/Subtitles.js
+++ src/js/Subtitles.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { MdClosedCaption } from "react-icons/md";
+import { FaClosedCaptioning } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import ReactTooltip from "react-tooltip";
 import "../css/buttons.css";
 const Subtitles = props => {
-  console.log(props);
   return (
     <div
       onClick={props.handleSubtitles}
       className="subtitles"
       style={{ display: props.subtitlesBtn }}
+      data-tip={
+        props.subtitlesBtnState === "none" ? "Caption Off" : "Caption On"
+      }
+      data-effect="solid"
+      data-place="top"
     >
+      <ReactTooltip />
       <IconContext.Provider value={{ className: "wrapper hover" }}>
-        <MdClosedCaption />
+        {props.subtitlesBtnState === "none" ? (
+          <MdClosedCaption />
+        ) : (
+          <FaClosedCaptioning />
+        )}
       </IconContext.Provider>
       <div
         className="border"
